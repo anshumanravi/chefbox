05a411eb8e85864a2fa47b2c3fd2cc923b9ac14c
Anshuman Ravi <ravianshmsr08@gmail.com>
2015-12-31 15:30:04 +0530

    Added metadata

---
+++ site-cookbooks/magento/metadata.rb
@@ -0,0 +1,12 @@
+name             'magento'
+maintainer       'Anshuman Ravi'
+maintainer_email 'ravianshmsr08@gmail.com'
+license          'All rights reserved'
+description      'Installs/Configures devbox'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.3.0'
+
+depends 'ssh_known_hosts'
+# depends 'cron'
+
+supports 'ubuntu'
